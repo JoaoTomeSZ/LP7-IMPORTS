6b0711d72e77247ba6853139f50f9df2dc635e37
João Carlos <joaocarlos8221@gmail.com>
2025-05-18 03:06:47 -0300

    fix: Unnecessary Tailwind File

--- src/components/about/Sells.jsx
+++ src/components/about/Sells.jsx
@@ -1,25 +1,22 @@
-import arsenal from "/arsenal.jpeg"
-import inter from "/inter.jpeg"
-import psg from "/psg.jpeg"
-
+import flamengo from "/flamengo.jpeg"
+import palmeiras from "/palmeiras.jpeg"
+import santos from "/santos.jpeg"
 const Sells = () => {
+const images = [
+    flamengo,
+    palmeiras,
+    santos
+  ];
+  
   return (
     <div className="lg:flex hidden lg:flex-row flex-col max-w-full w-full lg:justify-between justify-evenly lg:items-start items-center px-2 lg:gap-0 gap-4">
-      <img
-        src={psg}
-        alt=""
-        className="h-[350px] w-[300px] rounded-2xl object-fill"
-      />
-      <img
-        src={inter}
-        alt=""
-        className="h-[350px] w-[300px] rounded-2xl object-fill"
-      />
-      <img
-        src={arsenal}
-        alt=""
-        className="h-[350px] w-[300px] rounded-2xl object-fill"
-      />
+      <ul className="w-full flex justify-between">
+        {images.map((src, index) => (
+          <li key={index}>
+            <img src={src} alt="" className="h-[380px] w-[350px] rounded-2xl object-cover"/>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
