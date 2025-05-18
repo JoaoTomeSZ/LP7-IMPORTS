29e3e88f9c4a5d779ba3eb09a68e268e3c1df6a8
João Carlos <joaocarlos8221@gmail.com>
2025-05-18 03:10:18 -0300

    fix: scrolling margin

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import HeroText from "./HeroText"
 
 const Hero = () => {
   return (
-    <div id='inicio' className="min-h-[calc(100vh-150px)] h-full flex justify-center items-center">
+    <div id='inicio' className="min-h-[calc(100vh-150px)] h-full flex justify-center items-center scroll-mt-20">
         <div>
             <HeroText/>
         </div>
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ import LargeScrenSlide from './LargeScreenSlide'
 
 const About = () => {
   return (
-    <div id='sobre' className='min-h-screen h-full max-w-full w-full p-5 justify-center items-center flex flex-wrap'>
+    <div id='sobre' className='min-h-screen h-full max-w-full w-full p-5 justify-center items-center flex flex-wrap scroll-mt-20'>
       <div className='flex lg:flex-row flex-col w-full h-full'>
         <div className='flex-col max-h-full h-full items-end justify-between max-w-full w-full flex-wrap'>
               <AboutText/>
