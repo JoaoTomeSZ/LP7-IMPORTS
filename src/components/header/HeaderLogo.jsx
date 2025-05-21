ce444eb4b7f4f903c66a7f491726adbf7e88417c
João Carlos <joaocarlos8221@gmail.com>
2025-05-20 23:18:50 -0300

    feat: Logo link to Initial Page

--- src/components/header/HeaderLogo.jsx
+++ src/components/header/HeaderLogo.jsx
@@ -1,8 +1,9 @@
 import Logo from "/lp7Logo.png"
+import {Link} from "react-router-dom"
 
 const HeaderLogo = () => {
   return (
-    <img src={Logo} alt="LeoP7 Imports Logo" className="max-h-[150px]"/>
+    <Link to="/"><img src={Logo} alt="LeoP7 Imports Logo" className="max-h-[150px]"/></Link>
   )
 }
 
